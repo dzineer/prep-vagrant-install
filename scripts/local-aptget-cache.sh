bc0891520c400eb85c2851bae81282d12789e600
Franklin Decker III <frank@dzineer.com>
2020-06-25 21:56:14 +0700

    Prep Vagrant install

--- scripts/local-aptget-cache.sh
+++ scripts/local-aptget-cache.sh
@@ -2,7 +2,7 @@
 
 echo ">>> Adding local apt-get cache server"
 
-echo "Acquire::http::Proxy \”http://192.168.22.22:3142\”;" >> /etc/apt/apt.conf.d/02proxy
+echo 'Acquire::http::Proxy "http://192.168.22.22:3142";' >> /etc/apt/apt.conf.d/02proxy
 
 sed 's/deb http:\/\/security.ubuntu.com\/ubuntu bionic-security main restricted/deb http:\/\/192.168.22.22:3142\/ubuntu bionic-security main restricted/g' /etc/apt/sources.list
 sed 's/deb http:\/\/security.ubuntu.com\/ubuntu bionic-security universe/deb http:\/\/192.168.22.22:3142\/ubuntu bionic-security universe/g' /etc/apt/sources.list
